dcc212d072f026bd362e3d2d1ef7ccdc4b8808e7
Marko <marko.cuk.888@gmail.com>
2022-03-07 21:18:33 +0100

    fixing bug "bad connection to postgre"

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ con = psycopg2.connect(database="payroll", user="postgres", password="emerus2705
 cursor = con.cursor()
 
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:emerus2705@localhost/lexus'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:emerus2705@localhost/payroll'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'Radnici'
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(30))
+    LastName = db.Column(db.String(30))
+    Satnica = db.Column(db.Float)
+   
+
+    def __init__(self, id, FirstName, LastName, Satnica):
+        self.id = id
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Satnica = Satnica
+       
