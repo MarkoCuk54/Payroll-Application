81fec3777efefd317d07aa91f8b8b85ad7eb0aed
Marko <marko.cuk.888@gmail.com>
2022-03-11 12:11:09 +0100

    add row too secound table after submit new employe

--- app.py
+++ app.py
@@ -102,8 +102,10 @@ def submitNoviRadnik():
         if id == '' or firstName == '' or lastName == "" or Satnica == "":
             return render_template('dodajRadnika.html', message='Molim vas popunite obavezna polja')
         try:
+            data1 = placaTablica(id, firstName, lastName,0,0,0,0,0,0,0,0,0,0,0,0)
             data = Feedback(id, firstName, lastName, Satnica,Odjel, Opis)
             db.session.add(data)
+            db.session.add(data1)
             db.session.commit()
             return render_template('success.html')
         except:
