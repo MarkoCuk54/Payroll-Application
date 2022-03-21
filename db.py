cc740045d7657353c52613a5041623aec3042a6a
Marko <marko.cuk.888@gmail.com>
2022-03-21 15:56:23 +0100

    clean code

--- db.py
+++ db.py
@@ -2,15 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
-
 app = Flask(__name__)
 con = psycopg2.connect(database="payroll", user="postgres", password="emerus2705", host="127.0.0.1", port="5432")
 cursor = con.cursor()
-app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:emerus2705@localhost/payroll'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Feedback(db.Model):
     __tablename__ = 'radnici'
