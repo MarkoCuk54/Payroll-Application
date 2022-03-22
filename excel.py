3c84e55a5c45323a6ddd57b79045f0f4289c7607
Marko <marko.cuk.888@gmail.com>
2022-03-22 10:31:00 +0100

    changes in the for loop

--- excel.py
+++ excel.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 excelFile = pd.read_excel (r'C:\Users\Marko\Documents\test.xlsx')
-for index, row in excelFile.head(n=15).iterrows():
+for index, row in excelFile.head().iterrows():
      print(index, row)
