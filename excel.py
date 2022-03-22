c00e0dc35b2c1009291321ad29f6e4db318e8311
Marko <marko.cuk.888@gmail.com>
2022-03-22 10:45:53 +0100

    get specific data from exel file

--- excel.py
+++ excel.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 excelFile = pd.read_excel (r'C:\Users\Marko\Documents\test.xlsx')
 for index, row in excelFile.head().iterrows():
-     print(index, row)
+     print(row[0], row[3], row[4])
