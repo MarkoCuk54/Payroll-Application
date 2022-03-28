3fce72f4ef38b8b613b093f738e96d7aebce38f1
Marko <marko.cuk.888@gmail.com>
2022-03-28 10:05:47 +0200

    adding var for all multiplicators

--- app.py
+++ app.py
@@ -96,6 +96,12 @@ def kalkulator():
 @app.route("/excelFile")
 def excelFile():
     try:
+        smjena3 = 1.5
+        ned1i2 = 1.15
+        dan7i8 = 1.5
+        vikendPrekovremeni = 1.5
+        blagdan = 1.3
+        bolovanje = 0.8
         satiIndex = 0
         # Ovdje imam sve IDove:
         ids = []
