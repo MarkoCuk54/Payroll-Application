a1b9251690aac28ea836730c12568e60cb82f7e6
Marko <marko.cuk.888@gmail.com>
2022-07-12 09:01:38 +0200

    finishing gorivo module

--- app.py
+++ app.py
@@ -211,43 +211,42 @@ def excelFile():
         prezimena = []
         jmbgs= []
         cijenaGoriva = []
-        # try:
-        excelFile = pd.read_excel (r'C:\Users\Marko\Documents\platnaLista.xlsx')
-        for index, row in excelFile.head(n = 50).iterrows():
-                ids.append(row["id"])
-                sati.append(row["sati"])
-                smjena3list.append(row["3.smjena"])
-                prekovremeni1i2list.append(row["prekovremeni 1 i 2"])
-                prvaIdruganedlist.append(row["1 i 2 ned"])
-                sedmiI8danlist.append(row["7.8 dan"])
-                prekovremeniVikendlist.append(row["prekovremeni vikend"])
-                blagdanlist.append(row["blagdan"])
-                bolovanjeList.append(row["bolovanje"])
-                bonusList.append(row["bonus"])
-                cijenaGoriva.append(row["cijenaGoriva"])
-        for id in ids:
-                cursor.execute("SELECT * FROM radnici where  id = " + str(id))
-                result = cursor.fetchall()
-                satnica = (result[0][3])
-                ime = result[0][1]
-                prezime = result[0][2]
-                jmbg = result[0][6]
-                kilometre = result[0][7]
-                gorivo = float(cijenaGoriva[satiIndex]) * 22 * 6.6 * float(kilometre) / 100
-                gorivo = round(gorivo,2)
-                print(gorivo)
-                placa = (float(satnica) * int(sati[satiIndex])) + (float(satnica) * int(smjena3list[satiIndex]) * smjena3) + (float(satnica) * int(prekovremeni1i2list[satiIndex])) + (float(satnica) * int(prvaIdruganedlist[satiIndex]) * ned1i2 ) + (float(satnica) * int(sedmiI8danlist[satiIndex]) * dan7i8) + (float(satnica) * int(prekovremeniVikendlist[satiIndex]) * vikendPrekovremeni) + (float(satnica) * int(blagdanlist[satiIndex]) * blagdan) + (float(satnica) * int(bolovanjeList[satiIndex]) * bolovanje) + int(bonusList[satiIndex]) + gorivo
-                placa = str(round(placa, 2))
-                place.append(placa)
-                imena.append(ime)
-                prezimena.append(prezime)
-                jmbgs.append(jmbg)
-                satiIndex += 1
-        return render_template("excelFile.html", dataIme = imena, dataPrezime = prezimena, dataPlaca = place, dataId = ids, dataJmbg = jmbgs)
-        # except:
-        #     cursor.execute("ROLLBACK")
-        #     con.commit()
-        #     return render_template("error.html", message = "Nest sa Excel Filom nije uredu!")
+        try:
+            excelFile = pd.read_excel (r'C:\Users\Marko\Documents\platnaLista.xlsx')
+            for index, row in excelFile.head(n = 50).iterrows():
+                    ids.append(row["id"])
+                    sati.append(row["sati"])
+                    smjena3list.append(row["3.smjena"])
+                    prekovremeni1i2list.append(row["prekovremeni 1 i 2"])
+                    prvaIdruganedlist.append(row["1 i 2 ned"])
+                    sedmiI8danlist.append(row["7.8 dan"])
+                    prekovremeniVikendlist.append(row["prekovremeni vikend"])
+                    blagdanlist.append(row["blagdan"])
+                    bolovanjeList.append(row["bolovanje"])
+                    bonusList.append(row["bonus"])
+                    cijenaGoriva.append(row["cijenaGoriva"])
+            for id in ids:
+                    cursor.execute("SELECT * FROM radnici where  id = " + str(id))
+                    result = cursor.fetchall()
+                    satnica = (result[0][3])
+                    ime = result[0][1]
+                    prezime = result[0][2]
+                    jmbg = result[0][6]
+                    kilometre = result[0][7]
+                    gorivo = float(cijenaGoriva[satiIndex]) * 22 * 6.6 * float(kilometre) / 100
+                    gorivo = round(gorivo,2)
+                    placa = (float(satnica) * int(sati[satiIndex])) + (float(satnica) * int(smjena3list[satiIndex]) * smjena3) + (float(satnica) * int(prekovremeni1i2list[satiIndex])) + (float(satnica) * int(prvaIdruganedlist[satiIndex]) * ned1i2 ) + (float(satnica) * int(sedmiI8danlist[satiIndex]) * dan7i8) + (float(satnica) * int(prekovremeniVikendlist[satiIndex]) * vikendPrekovremeni) + (float(satnica) * int(blagdanlist[satiIndex]) * blagdan) + (float(satnica) * int(bolovanjeList[satiIndex]) * bolovanje) + int(bonusList[satiIndex]) + gorivo
+                    placa = str(round(placa, 2))
+                    place.append(placa)
+                    imena.append(ime)
+                    prezimena.append(prezime)
+                    jmbgs.append(jmbg)
+                    satiIndex += 1
+            return render_template("excelFile.html", dataIme = imena, dataPrezime = prezimena, dataPlaca = place, dataId = ids, dataJmbg = jmbgs, dataGorivo = gorivo)
+        except:
+            cursor.execute("ROLLBACK")
+            con.commit()
+            return render_template("error.html", message = "Nest sa Excel Filom nije uredu!")
 
 @app.route("/excelMjesec", methods=["GET", "POST"])
 def excelMjesec():
@@ -287,7 +286,9 @@ def excelMjesec():
                 result = cursor.fetchall()
                 satnica = (result[0][3])
                 kilometre = result[0][7]
-                placa = (float(satnica) * int(sati[satiIndex])) + (float(satnica) * int(smjena3list[satiIndex]) * smjena3) + (float(satnica) * int(prekovremeni1i2list[satiIndex])) + (float(satnica) * int(prvaIdruganedlist[satiIndex]) * ned1i2 ) + (float(satnica) * int(sedmiI8danlist[satiIndex]) * dan7i8) + (float(satnica) * int(prekovremeniVikendlist[satiIndex]) * vikendPrekovremeni) + (float(satnica) * int(blagdanlist[satiIndex]) * blagdan) + (float(satnica) * int(bolovanjeList[satiIndex]) * bolovanje) + int(bonusList[satiIndex]) + (((float(cijenaGoriva[satiIndex]) * 22 * 6.6) * (float[kilometre] / 100)))
+                gorivo = float(cijenaGoriva[satiIndex]) * 22 * 6.6 * float(kilometre) / 100
+                gorivo = round(gorivo,2)
+                placa = (float(satnica) * int(sati[satiIndex])) + (float(satnica) * int(smjena3list[satiIndex]) * smjena3) + (float(satnica) * int(prekovremeni1i2list[satiIndex])) + (float(satnica) * int(prvaIdruganedlist[satiIndex]) * ned1i2 ) + (float(satnica) * int(sedmiI8danlist[satiIndex]) * dan7i8) + (float(satnica) * int(prekovremeniVikendlist[satiIndex]) * vikendPrekovremeni) + (float(satnica) * int(blagdanlist[satiIndex]) * blagdan) + (float(satnica) * int(bolovanjeList[satiIndex]) * bolovanje) + int(bonusList[satiIndex]) + gorivo
                 placa = str(round(placa, 2))
                 place.append(placa)
                 sql_update_query = "Update placamjesecna set " + mjesec +" = %s where id = %s"
